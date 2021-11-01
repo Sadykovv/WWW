5888d255bbedec731a003526c33286014fb68fcb
Alisher <alisher.sadykov.95@mail.ru>
2021-11-01 23:44:33 +0400

    Добавление в перечень задачи 1.1 из самостоятельной работы

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 <ul>
         <li><a href="lab-1-1.php">Упражнение 1-2</a></li>
 	<li><a href="lab-1-2.php">Упражнение 1-3</a></li> 
-	<li><a href="lab-1-3.php">Упражнение 1-4</a></li>	
+	<li><a href="lab-1-3.php">Упражнение 1-4</a></li>
+	<li><a href="lab1-5.php">Задача 1-1</a></li>	
 </ul>
