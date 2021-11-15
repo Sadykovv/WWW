95d36d63f51c3a38d7643e39241f2f3488301de8
Alisher <alisher.sadykov.95@mail.ru>
2021-11-15 19:15:34 +0400

    Сделал задание 2-3

--- lab-2-3.php
+++ lab-2-3.php
@@ -1,4 +1,31 @@
 <?php 
+echo "<p style ='color:blue'>Задание ко второй лабораторной работе</p>";
+echo "Массив:";
+$cust=array(
+'cnum'=>2001, 
+'cname' => 'Hoffman',
+'city' => 'London',
+'snum' =>1001
+);
+print_r($cust);
+echo "<br>";
+echo "Массив с ключом rating:";
+$cust['rating']=100;
+print_r($cust);
+echo "<br>";
+echo "Отсортированный массив по значениям:";
+asort($cust);
+print_r($cust);
+echo "<br>";
+echo "Отсортированный массив по ключам:";
+ksort($cust);
+print_r($cust);
+echo "<br>";
+echo"Сортировка массива:";
+sort($cust);
+print_r($cust);
+echo "<br>";
+echo "<p style ='color:blue'>Задание к первой лабораторной работе</p>";
 $а = "А";  
 switch ($а) {  
  case 'А' : 
