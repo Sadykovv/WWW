10d6c7997a35ec31b742328198c59e000616dd8a
Alisher <alisher.sadykov.95@mail.ru>
2021-11-15 18:59:25 +0400

    Сделал задание №2-2

--- lab-2-2.php
+++ lab-2-2.php
@@ -1,14 +1,34 @@
 <?php 
-echo "Задание ко второй лабораторной работе","<br>";
-echo '1)';
+echo "<p style ='color:blue'>Задание ко второй лабораторной работе</p>";
 $x=rand(3,20);
 $arr=array();
+echo "Массив из" . " " .$x. "-ми/-ти/-х/-го ". "элементов, заполненный случайными числами: ";
 for ($i=0;$i<$x;$i++){
 	$arr[$i]=rand(10,99);
 }
+print join(" ",$arr)."<br>";
+echo "Отсортированный массив:";
+sort($arr);
+print join(" ",$arr)."<br>";
+echo "Реверс массива:";
+print join(" ",array_reverse($arr))."<br>";
+echo "Массив с удаленным последним элементом:";
+array_pop($arr);
+print join(" ",$arr)."<br>";
+echo "Подсчет суммы элементов в массиве и количества элементов:";
+echo array_sum($arr).";".count($arr)."<br>";
+echo "Вывод среднего арифметического:";
+echo array_sum($arr)/count($arr)."<br>";
+echo "Определение значения в массиве:";
+if(in_array(50,$arr)){
+echo "Нашел"."<br>";
+} else {
+echo "Не нашел"."<br>";
+}
+echo "Удаление повторяющихся элементов:";
+$arr=array_unique($arr);
 print join(" ",$arr);
-echo "<br>";
-echo "Задание к первой лабораторной работе","<br>";
+echo "<p style ='color:blue'>Задание к первой лабораторной работе</p>";
 $x=rand(1,10);   
 $y=rand(1,10); 
 print ('$x =' . $x . "<br>"); 
