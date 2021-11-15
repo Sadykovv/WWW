8657c685036d9f920c93eb87e7e54d992118dc7c
Alisher <alisher.sadykov.95@mail.ru>
2021-11-15 16:05:01 +0400

    Ну сделал задание №2-1.

--- lab-2-1.php
+++ lab-2-1.php
@@ -1,4 +1,14 @@
 <?php 
+$treug=array();
+$kvd=array();
+$rez=array();
+$rez1=array();
+echo '1)';
+for ($n=1;$n<=10;$n++){
+	$treug[$n-1]=$n*($n+1)/2;
+	echo $treug[$n-1]." ";
+}
+echo "<br>";
 $a=-1;   $b = "";  
 if ($a)  {  echo '$a='."$a - истина<br>";  } 
   else {  echo '$a='."$a - ложь<br>";  }  
