5414079897f6d3340bfb9cffce9a605b7083c360
Alisher <alisher.sadykov.95@mail.ru>
2021-11-22 17:45:19 +0400

    Добавил ф6_п.хтмл

--- index.php
+++ index.php
@@ -29,4 +29,5 @@
         <li><a href="f3.html">Упражнение 3-3</a></li> 	
         <li><a href="f4.html">Упражнение 3-4</a></li>
         <li><a href="f5.html">Упражнение 3-5</a></li>
+        <li><a href="f6.html">Упражнение 3-6</a></li>
 </ul>
