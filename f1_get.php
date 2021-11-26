14b6d4b42cb55195081874f86b350f0119350a18
Alisher <alisher.sadykov.95@mail.ru>
2021-11-26 15:01:12 +0400

    Подправил

--- f1_get.php
+++ f1_get.php
@@ -2,7 +2,7 @@
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 </HEAD>
 <?php 
- echo ("Здравствуйте, " . $_GET["username"]); 
+ echo ("Здравствуйте, " . $_GET["userName"]); 
  echo ("<hr>"); 
  echo ("Значение скрытого поля hideField равно " . 
 $_GET["hideField"]); 
